c6877809b0b4c2df48252ef0b9525658977922d8
Artem Medeu <artem.medeusheyev@gmail.com>
2022-08-21 17:35:56 +0600

    deployment

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b master
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:armed/cherry-hn-reader.git master:gh-pages
+
+cd -
